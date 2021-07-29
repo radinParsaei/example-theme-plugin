e2b9a65e5a0769eadf3f37a1d15110557e40578c
radin parsaei <radinparsaei@gmail.com>
2021-07-29 17:30:01 +0430

    Update example-theme-plugin.js

--- example-theme-plugin.js
+++ example-theme-plugin.js
@@ -1,2 +1,5 @@
-// remove grid
-setBlocksEditorGrid(undefined)
+// Remove grid
+Editor.setBlocksEditorGrid(undefined)
+
+// Change console category color to red(ff0000)
+Editor.setCategoryAndItsBlocksColor('console', 'ff0000')
