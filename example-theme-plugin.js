eaaf90933d664c06210c025ea8511ab6f13989ce
radin parsaei <radinparsaei@gmail.com>
2021-08-06 22:54:10 +0430

    Update example-theme-plugin.js

--- example-theme-plugin.js
+++ example-theme-plugin.js
@@ -3,3 +3,6 @@ Editor.setBlocksEditorGrid(undefined)
 
 // Change console category color to red(ff0000)
 Editor.setCategoryAndItsBlocksColor('console', 'ff0000')
+
+// The next line changes the color of predefined objects in the text editor to ff5555
+Editor.setTextEditorColor('predefined', '#ff5555')
